f60390019a347f80a42de94ffc44252bd4a7b826
Joshua Navarro <joshua.navarro35@gmail.com>
2018-02-18 17:22:48 -0500

    tweak Google OAuth

--- services/passport.js
+++ services/passport.js
@@ -20,7 +20,8 @@ passport.deserializeUser((id, done) => {
 passport.use(new GoogleStrategy({
   clientID: keys.googleClientID,
   clientSecret: keys.googleClientSecret,
-  callbackURL: '/auth/google/callback'
+  callbackURL: '/auth/google/callback',
+  proxy: true
 }, (accessToken, refleshToken, profile, done) => {
   User.findOne({ googleId: profile.id })
     .then((existingUser) => {
