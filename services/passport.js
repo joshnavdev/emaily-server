a49ceffb763310ae4132a41d5fd8ad80a8c0046e
Joshua Navarro <joshua.navarro35@gmail.com>
2018-02-19 03:06:04 -0500

    Start with the client side

--- services/passport.js
+++ services/passport.js
@@ -22,16 +22,16 @@ passport.use(new GoogleStrategy({
   clientSecret: keys.googleClientSecret,
   callbackURL: '/auth/google/callback',
   proxy: true
-}, (accessToken, refleshToken, profile, done) => {
-  User.findOne({ googleId: profile.id })
-    .then((existingUser) => {
-      if (existingUser) {
-        done(null, existingUser);
-      } else {
-        new User({ googleId: profile.id }).save()
-          .then(user => done(null, user));
-      }
-    });
+}, async (accessToken, refleshToken, profile, done) => {
+  const existingUser = await User.findOne({ googleId: profile.id });
+
+  if (existingUser) {
+    return done(null, existingUser);
+  }
+
+  const user = await new User({ googleId: profile.id }).save();
+  done(null, user);
+
 }));
 
 passport.use(new FacebookStrategy({
